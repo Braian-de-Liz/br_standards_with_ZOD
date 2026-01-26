8d418f5c45673d5593d63613c49b51ca1ae01ac1
Braian-de-Liz <delizbraian@gmail.com>
2026-01-25 22:11:06 -0300

    configs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { z } from "zod";
-import { describe, it, expect } from 'vitest';
 import { Valid_CPF } from "./validations/cpf.js";
 import { Validation_cnpj } from "./validations/cnpj.js";
 
 
+const zbr = {
+
+    cpf: (msg?: string) => z.string()
+        .transform(v => v.replace(/\D/g, ""))
+        .refine(val => val.length === 11, "CPF deve ter 11 dígitos")
+        .refine(Valid_CPF, { message: msg || "CPF inválido" }),
+
+    cnpj: (msg?: string) => z.string()
+        .transform(v => v.replace(/\D/g, ""))
+        .refine(val => val.length === 14, "CNPJ deve ter 14 dígitos")
+        .refine(Validation_cnpj, { message: msg || "CNPJ inválido" }),
+};
+
+
+export { zbr };
+export default zbr;
